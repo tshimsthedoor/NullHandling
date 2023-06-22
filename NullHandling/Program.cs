1d711440dbd64d5b122b672a20a23adc12725212
tresor <tshims79@gmail.com>
2023-06-22 12:42:00 +0200

    initiate a value null then give it the value of 7 then run it.

--- NullHandling/Program.cs
+++ NullHandling/Program.cs
@@ -6,7 +6,16 @@ namespace NullHandling
     {
         static void Main(string[] args)
         {
-            
+            int thisCannotBeNull = 4;
+            //thisCannotBeNull = null; //compile error
+
+            int? thisCouldBeNull = null;
+            Console.WriteLine(thisCouldBeNull);
+            Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+            thisCouldBeNull = 7;
+            Console.WriteLine(thisCouldBeNull);
+            Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
         }
     }
 }
